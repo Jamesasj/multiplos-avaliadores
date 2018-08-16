76044ba42b1829d9009af034fc1177271a9e638a
James <james@jamesalves.com.br>
2018-08-16 15:09:28 -0300

    analise da variancia com N avaliadores

--- Programa.R
+++ Programa.R
@@ -3,22 +3,28 @@ library(dplyr)
 library(KappaGUI)
 
 setwd("C:\\PROJETOS\\avaliacao_pares_professor")
-lista_doc <- c("professor1.csv", "professor2.csv")
+lista_doc <- c("professor1.csv", "professor2.csv", "professor3.csv")
 
-dados1 <- read.csv("professor1.csv", sep = ";")
-dados2 <- read.csv("professor2.csv", sep = ";")
-
-varl <- data_frame(x = dados2[,7])
-var2 <- data_frame(x = dados1[,7])
-scala <- data.frame(x =c(0:1000,100))
 jpeg("01_GERAL.jpg")
 plot <- ggplot()
-plot + 
-  stat_function(data = varl, aes(x = scala),fun = dnorm, args = list(mean(varl$x), sd(varl$x)), colour = "blue") +
-  geom_vline(xintercept = mean(varl$x), colour = "blue",  linetype = "dashed") +
-  stat_function(data = var2, aes(x = scala),fun = dnorm, args = list(mean(var2$x), sd(var2$x)), colour = "red")  +
-  geom_vline(xintercept = mean(var2$x), colour = "red", linetype = "dashed") 
+for (documento in lista_doc) {
+  cor1 <- sample(1:100,1)
+  dados <- read.csv(documento,sep = ";")
+  varl <- data_frame(x = dados[,7])
+  scala <- data.frame(x =c(0:1000,100))
+  
+  plot <- plot + 
+    stat_function(data = varl, aes(x = scala),fun = dnorm, args = list(mean(varl$x), sd(varl$x)), colour = cor1) +
+    geom_vline(xintercept = mean(varl$x), colour = cor1,  linetype = "dashed")
+
+}
+plot
 dev.off()
+
+
+##### Analise cohen kappa #######
+dados1 <- read.csv("professor1.csv", sep = ";")
+dados2 <- read.csv("professor2.csv", sep = ";")
 
 dadosC <- data.frame(professor1 = as.character(), professor2 =as.character(), nota = as.character(), kappa=as.numeric())
 
